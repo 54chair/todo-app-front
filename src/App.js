715b9350759cedcb93a20fb1fb6810f188cefa6d
54chair <takukoshi3721@gmail.com>
2023-09-13 13:40:22 +0900

    Taskコンポーネントの追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Task from './component/Task';
+
 const App = () => {
   return (
-    <h1>これはテストです</h1>
+    <>
+      <h1>タスク一覧</h1>
+      <Task name="買い物" />
+      <Task name="ランニング" />
+      <Task name="プログラミングの勉強" />
+    </>
   );
 }
 
